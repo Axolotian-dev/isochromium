ddc6dc24acd1c7f9a2fe46b9ce430d1386bfd57e
Axolotian-dev <dynamical.dev@gmail.com>
2025-02-24 13:29:52 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Isolated Chromium on Vercel</h1>
+      <p>Test the Chromium instance by calling the API route:</p>
+      <pre>curl /api/browser</pre>
+    </div>
+  );
+}
